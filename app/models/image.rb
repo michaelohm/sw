acf696af4d1ab59670d25c625849de1d9f3f5f79
Michael Ohm <michael.k.ohm@gmail.com>
2014-07-05 17:00:09 -0700

    ignore aws file

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
 	has_attached_file :attachment,
 										:storage => :s3,
 										:s3_host_name => 's3-us-west-2.amazonaws.com',
-										:s3_credentials => Proc.new{|a| a.instance.s3_credentials}
+										:s3_credentials => "#{Rails.root}/config/aws.yml";
 
 
 # Validate content type
@@ -12,12 +12,6 @@ class Image < ActiveRecord::Base
   validates_attachment_file_name :attachment, :matches => [/png\Z/, /jpe?g\Z/]
 # Explicitly do not validate
   do_not_validate_attachment_file_type :attachment
-
-
-
-  def s3_credentials
-  	{:bucket => 'selfiewars', :access_key_id => 'AKIAIN4EMOO4HIUG6MYA', :secret_access_key => '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'}
-  end
 
 
 end
